e5ea62c9f28905ddda51d6e1b3eb914ff30de12f
Julia Kristensen <julia.kristensen05@gmail.com>
2025-05-31 11:23:58 -0700

    plotting

--- ploting.py
+++ ploting.py
@@ -5,6 +5,7 @@ MODE_NAME = "base"
 # MODE_NAME = "tariff25"
 
 df = pd.read_csv(f"risk_scores_{MODE_NAME}.csv")
+df2 = pd.read_excel("data/BC Hydro - Category Description.xlsx")
 
 # Sort so the highest-risk items land at the top of the chart
 df_sorted = df.sort_values("scenario_risk_score", ascending=True)
